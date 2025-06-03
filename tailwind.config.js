d4bb145b449d1c8850231c70872728769f60803a
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-06-02 23:24:07 -0300

    Downgrade tailwind to v3

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,jsx,tsx,mdx}',
+    './src/components/**/*.{js,jsx,tsx,mdx}',
+    './src/app/**/*.{js,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
