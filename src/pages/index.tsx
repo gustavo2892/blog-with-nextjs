b2622e998bfa796b8e00a11451d32c4d244e86ad
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-06-04 19:34:12 -0300

    Create feature section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import { HeroSection } from '@/components/hero-section';
-
-
+import { FeatureSection } from '@/components/feature-section';
 
 export default function Home() {
   return (
     <>
     <article className="flex flex-col">
       <HeroSection />
+      <FeatureSection />
     </article>
   </>
   );
