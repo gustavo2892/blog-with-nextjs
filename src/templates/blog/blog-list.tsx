cf40f5ab2ba8b0b36c64c7ef72236f57e06a4c80
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-07-18 22:42:58 -0300

    Add SSG

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,24 +1,28 @@
 import { Search } from '@/components/search';
-import { allPosts } from 'contentlayer/generated';
+import { Post } from 'contentlayer/generated';
 import { Inbox } from 'lucide-react';
 import { useRouter } from 'next/router';
 import { PostCard } from './components/post-card';
 import { PostGridCard } from './components/post-grid-card';
 
-export function BlogList() {
+export type BlogListProps = {
+  posts: Post[];
+};
+
+export function BlogList({ posts }: BlogListProps) {
   const router = useRouter();
   const query = router.query.q as string;
   const pageTitle = query
     ? `Resultados de busca para "${query}"`
     : 'Dicas e estratégias para impulsionar seu negócio';
 
-  const posts = query
-    ? allPosts.filter((post) =>
-        post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
-      )
-    : allPosts;
+  const postList = query
+    ? posts.filter((post) =>
+      post.title.toLocaleLowerCase()?.includes(query.toLocaleLowerCase())
+    )
+  : posts;
 
-  const hasPosts = posts?.length > 0;
+  const hasPosts = postList?.length > 0;
 
   return (
     <div className="flex flex-col py-24 flex-grow h-full">
@@ -43,7 +47,7 @@ export function BlogList() {
       {/* Listagem de posts */}
       {hasPosts && (
         <PostGridCard>
-          {posts.map((post) => (
+          {postList.map((post) => (
             <PostCard
               key={post._id}
               title={post.title}
