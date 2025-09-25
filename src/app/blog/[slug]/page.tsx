25de593eaaba4795f0dad877349eebe3c2224c03
Gustavo Vidal de Freitas <gustavo2892@gmail.com>
2025-09-24 21:48:23 -0300

    Add more metadata in post page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,35 @@
 import { PostPage } from '@/templates/blog';
 import { allPosts } from 'contentlayer/generated';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image],
+    },
+  };
+}
+
 
 export const revalidate = 60;
 
@@ -17,7 +40,7 @@ export async function generateStaticParams() {
 }
 
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
 
